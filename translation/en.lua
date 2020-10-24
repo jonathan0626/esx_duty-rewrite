8d1662ae81be42ce4f1487935f9e7455ebb158bf
Jonathan Peng <67518082+jonathan0626@users.noreply.github.com>
2020-10-25 00:55:55 +0800

    Update en.lua

--- translation/en.lua
+++ translation/en.lua
@@ -1,8 +1,7 @@
 Locales['en'] = {
-  -- regulars
-  	['duty'] = '按 ~g~E~s~ ~g~上班~s~/~r~下班~s~',
-	['onduty'] = '你已打卡上班',
-	['offduty'] = '你已打卡下班',
-	['notpol'] = '你不是警察',
-	['notamb'] = '你不是醫護',
+  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~emter~s~/~r~exit~s~ duty',
+	['onduty'] = 'You went onduty.',
+	['offduty'] = 'You went offduty.',
+	['notpol'] = 'You are not a policemen.',
+	['notamb'] = 'You are not a doctor.',
 }
